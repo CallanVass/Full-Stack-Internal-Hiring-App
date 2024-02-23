3b2ec7932b86dfc545c9b1dd8ace22a7dc26d6db
Callan <callan.v@fake.com>
2024-02-24 08:59:35 +1100

    Created get routes for user-search

--- routes/user_search.js
+++ routes/user_search.js
@@ -1,1 +1,32 @@
-// User search page route
+import { Router } from "express"
+import {bcrypt} from "bcrypt"
+import {UserModel} from '../db.js'
+
+const router = Router()
+
+// Get all users
+router.get('/users', async (req, res) => {
+  try {
+    const users = await UserModel.find({})
+    res.status(200).send(users)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+
+// Get a single user by ID
+router.get('/users/:id', async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.id)
+    if (!user) {
+      return res.status(404).send()
+    }
+    res.send(user)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+
+export default router
