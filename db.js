0c7819f598a8af09f59fd04ba184f1ed9957b905
Kelly <kelly.degannes@gmail.com>
2024-02-23 20:27:33 +1100

    Further DB setup and seed data

---
+++ db.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+try {
+    const m = await mongoose.connect(process.env.DB_URI)
+    console.log(m.connection.readyState === 1 ? 'MongoDB connected!' : 'MongoDB failed to connect')
+}
+catch (err) {
+    console.error(err)
+}
+
+const closeConnection = () => {
+    console.log('Mongoose disconnecting ...')
+    mongoose.disconnect()
+}
+
+const usersSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true },
+    department: { type: String , required: true },
+    admin: { type: Boolean, required: true },
+    aboutMe: { type: Object, required: false }, // Object with key-value pairs: headline, career_goals, tags etc
+    applications: {type: mongoose.objectId, ref: 'Listing'}
+})
+
+const UserModel = mongoose.model('User', usersSchema)
+
+
+const listingsSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: Object, required: true }, // Includes bullet points and full text
+    department: { type: String, required: true },
+    location: { type: String, required: true },
+    roleType: { type: String, required: true },
+    roleDuration: { type: String, required: true },
+    salary: { type: Number, required: true },
+    datePosted: { type: Date, required: true },
+    dateClosing: { type: Date, required: true },
+    newListing: { type: Boolean, required: true, default: true},
+    listingStatus: { type: String, required: true, default: 'Active'},
+    applicants: {type: mongoose.objectId, ref: 'User'},
+    creator : {type: mongoose.objectId, ref: 'User'}
+
+})
+
+const ListingModel = mongoose.model('Listing', listingsSchema)
+
+
+export { closeConnection, UserModel, ListingModel }
