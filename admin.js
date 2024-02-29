0d48ba290d6e935f1bed6a31eb9784f45b59a503
Callan <callan.v@fake.com>
2024-02-29 14:38:36 +1100

    Trying admin auth

---
+++ admin.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken'
+import { UserModel } from './db.js'
+
+const adminAuth = async (req, res, next) => {
+    try {
+        const authHeader = req.header('Authorization')
+        if (!authHeader) {
+            throw new Error('Authorization header is missing')
+        }
+
+        const token = authHeader.replace('Bearer ', '')
+        const decoded = jwt.verify(token, process.env.SECRET_KEY)
+        const user = await UserModel.findOne({ _id: decoded._id })
+
+        if (!user) {
+            throw new Error('User not found')
+        }
+
+        if (user.isAdmin === false) {
+            throw new Error('Access denied. Admin privileges required.')
+        }
+
+        req.user = user
+        next()
+    } catch (error) {
+        res.status(403).send({ error: 'Access denied. Please authenticate as an admin.' })
+    }
+}
+
+export default adminAuth
